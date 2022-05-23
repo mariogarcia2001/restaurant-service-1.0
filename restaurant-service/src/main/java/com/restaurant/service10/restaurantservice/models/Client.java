f2d0cc2d4936dec69f1fa540fed6c1563bb59786
mariogarcia2001 <mdgarcia15@espe.edu.ec>
2022-05-22 23:15:39 -0500

    Update 2 22/05

--- restaurant-service/src/main/java/com/restaurant/service10/restaurantservice/models/Client.java
+++ restaurant-service/src/main/java/com/restaurant/service10/restaurantservice/models/Client.java
@@ -4,11 +4,20 @@ import java.net.PasswordAuthentication;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="TBL_CLIENT")
+@Getter
+@Setter
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
